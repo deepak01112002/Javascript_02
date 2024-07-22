df0c8e0e171cc012b07c1eedf76b7bc1bbb5c955
deepak01112002 <deepak8574757609@gmail.com>
2024-07-22 15:10:05 +0530

    day 5 if else

---
+++ Day2/script.js
@@ -0,0 +1,48 @@
+
+
+
+// Arthmetical Operator:-
+// + , - , * , / , % , **
+
+
+let a = 2;
+var b = 2;
+
+let sum = a ** b
+
+
+document.getElementById("data").innerHTML = sum;
+
+
+let z = 25;
+
+  z = z - 5;  // z += 5  //30
+  z %= 5   // 35
+
+
+//   console.log(z)
+  let name = "deepak"
+  document.getElementById("deepak").innerHTML = `<h1>Answer of z + 10 = ${z}</h1>`
+
+
+
+
+// Comparison Operators
+
+    // == or ===
+
+    let ram  = 25
+    let shyam = "25";
+    // console.log(ram == shyam)
+
+
+
+    // console.log(true && !false && !false && true)
+
+
+    let isAvailable = t
+    
+    
+
+
+
---
+++ day5/script.js
@@ -0,0 +1,52 @@
+
+
+// let time = "7 : 00"
+
+// if(time == "7 :00"){
+//     console.log("Ghar aa Jana")
+// }else{
+//     console.log("Call Me")
+// }
+
+
+// let age  = 20;
+
+// if(age >= 18){
+//     console.log("Eligible to Vote")
+// }else{
+//     console.log("Not Eligible")
+// }
+
+// if(age < 18){
+//     console.log("Not Eligible")
+// }else{
+//     console.log("Eligible to Vote")
+// }
+function deepak(){
+
+
+// let age = document.getElementById("age").value
+// if(age < 18){
+//     alert("Not Eligible")
+// }else{
+//     alert("Eligible to Vote")
+// }
+
+let a = document.getElementById("a").value
+let b = document.getElementById("b").value
+
+if(a > b){
+    alert(`${a} is greater than ${b}`)
+}else if(a == b){
+    alert(`${b} is equal to ${a}`)
+}else if(a<b){
+    alert(`${b} is greater than ${a}`)
+}
+    
+
+
+
+
+}
+
+
